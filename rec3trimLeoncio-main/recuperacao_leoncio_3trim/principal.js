d1d1a20a3ce3f9a937958c6634a799f20ff87242
nathanmedeiros700444 <130366446+nathanmedeiros700444@users.noreply.github.com>
2023-11-06 09:27:44 -0300

    Add files via upload

---
+++ rec3trimLeoncio-main/recuperacao_leoncio_3trim/principal.js
@@ -0,0 +1,17 @@
+// DAQUI PRA BAIXO COMEÇA O SITE
+console.log("Bom dia!")   
+
+//
+LetNome=document.querySelector(".nome");
+LetNome=td.nometextContent
+
+//
+LetIdade=document.querySelector(".idade");
+LetIdade=td.idadetextContent
+
+// CALCULANDO A IDADE PERMITIDA
+LetidadeJogador= idade = idadepermitida;
+console.log("idade = idade permitida");
+tdmedia.textContent= idadePermitida;
+
+
